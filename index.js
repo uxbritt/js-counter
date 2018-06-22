a6808d19934adb58e3a5cdace16a67abf1772343
bdemers1055 <bdemers1055@gmail.com>
2018-06-22 19:27:31 -0400

    refactored to utilize an object literal as the container for our app

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
 
-//variables
-const app = document.querySelector('#app');
-const addBtn = app.querySelector('.inc');
-const minusBtn = app.querySelector('.sub');
-const display = app.querySelector('.display');
 
-//eventListeners
-addBtn.addEventListener('click', addToCount);
-minusBtn.addEventListener('click', subFromCount);
+const App = {
+  count: 0,
+  init(){
+    this.cacheDom();
+    this.bindEventListeners();
+    this.render();
+  },
+  cacheDom(){
+    this.root = document.querySelector('#app');
+    this.addBtn = document.querySelector('.inc');
+    this.minusBtn = document.querySelector('.sub');
+    this.display = document.querySelector('.display');
+  },
+  bindEventListeners(){
+    this.addBtn.addEventListener('click', this.addToCount.bind(this));
+    this.minusBtn.addEventListener('click', this.subFromCount.bind(this));
+  },
+  addToCount(){
+    this.count += 1;
+    this.render();
+  },
+  subFromCount(){
+    this.count -= 1;
+    this.render();
+  },
+  render(){
+    this.display.textContent = this.count;
+    // console.log(this);
+  }
+}
 
-//functions
-function addToCount(){
-  const count = parseInt(display.textContent, 10);
-  display.textContent = count + 1;
-}
-function subFromCount(){
-  const count = parseInt(display.textContent, 10);
-  display.textContent = count - 1;
-}
+App.init();
+window.App = App;
+
