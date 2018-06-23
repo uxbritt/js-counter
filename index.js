b9bed44126a6450ab619c85fc8abe851c4b8b389
bdemers1055 <bdemers1055@gmail.com>
2018-06-23 17:38:55 -0400

    increase or decrease incrementally based on user input

--- index.js
+++ index.js
@@ -10,31 +10,20 @@ const App = {
   cacheDom(){
     this.root = document.querySelector('#app');
     this.addBtn = document.querySelector('.inc1');
-    this.addTwoBtn = document.querySelector('.inc2');
     this.minusBtn = document.querySelector('.sub1');
-    this.minusTwoBtn = document.querySelector('.sub2');
     this.display = document.querySelector('.display');
+    this.numb = document.querySelector(parseInt(numb.value));
   },
   bindEventListeners(){
     this.addBtn.addEventListener('click', this.addToCount.bind(this));
     this.minusBtn.addEventListener('click', this.subFromCount.bind(this));
-    this.addTwoBtn.addEventListener('click', this.addTwoToCount.bind(this));
-    this.minusTwoBtn.addEventListener('click', this.subTwoFromCount.bind(this));
   },
   addToCount(){
-    this.count += 1;
+    this.count += numb.value;
     this.render();
   },
   subFromCount(){
-    this.count -= 1;
-    this.render();
-  },
-  addTwoToCount(){
-    this.count += 2;
-    this.render();
-  },
-  subTwoFromCount(){
-    this.count -= 2;
+    this.count -= numb.value;
     this.render();
   },
   render(){
